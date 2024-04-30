639d0312cf860a8d32911970886950eb3bfef3ee
Giascar <giascar82@gmail.com>
2024-04-30 18:05:26 +0200

    added select

--- TDatabase/Queries/DocumentDbHelper.cs
+++ TDatabase/Queries/DocumentDbHelper.cs
@@ -1,4 +1,5 @@
-﻿using Shared;
+﻿using System.Data.Common;
+using Shared;
 using TDatabase.Database;
 using DB = TDatabase.Database.DbCsclAxteriscoContext;
 using DocumentModel = Shared.DocumentModel;
@@ -13,14 +14,41 @@ public class DocumentDbHelper
 
         if (idDocument > 0)
         {
-            documents = from d in documents
-                        where d.Id == idDocument
-                        select d;
+            documents = documents.Where(d => d.Id == idDocument);
         }
 
+        var docs = (from d in documents
+                    where d.Id == idDocument
+                    select new DocumentModel()
+                    {
+                        Categories = (from r in (from qc in db.QuestionChosens
+                                                 from q in db.Questions
+                                                 where qc.IdDocument == d.Id
+                                                 && qc.Id == q.Id
+                                                 group q by q.IdCategory into q2
+                                                 select new { Id = q2.First().IdCategory })
 
+                                      from c in db.Categories
+                                      where c.Id == r.Id
+                                      select new CategoryModel()
+                                      {
+                                          Id = c.Id,
+                                          Text = c.Text,
+                                          Questions = (from qc in db.QuestionChosens
+                                                       from q in db.Questions
+                                                       where qc.IdDocument == d.Id
+                                                       && qc.Id == q.Id
+                                                       select new QuestionModel()
+                                                       {
+                                                           Id = qc.Id,
+                                                           Text = q.Text,
+                                                           //TODO aggiungere le risposte
+                                                       }).ToList()
+                                      }).ToList(),
 
-        return [];
+                    }).ToList();
+
+        return docs;
     }
 
     public static async Task<int> Insert(DB db, DocumentModel document)
@@ -40,7 +68,7 @@ public class DocumentDbHelper
 
             db.Documents.Add(newDocument);
 
-            var nextQuestionChosenId =  (db.QuestionChosens.Any() ? db.QuestionChosens.Max(x => x.Id) : 0) + 1; 
+            var nextQuestionChosenId = (db.QuestionChosens.Any() ? db.QuestionChosens.Max(x => x.Id) : 0) + 1;
 
             foreach (var c in document.Categories)
             {
@@ -68,62 +96,62 @@ public class DocumentDbHelper
     }
 
     public static async Task<List<int>> Update(DB db, List<DocumentModel> documents)
+    {
+        List<int> modified = [];
+        try
         {
-            List<int> modified = [];
-            try
+            foreach (var elem in documents)
             {
-                foreach (var elem in documents)
+                var d = db.Documents.Where(x => x.Id == elem.Id).FirstOrDefault();
+                if (d is not null)
                 {
-                    var d = db.Documents.Where(x=>x.Id == elem.Id).FirstOrDefault();
-                    if (d is not null)
+                    // d.IdCategory = elem.IdCategory;
+                    // d.IdSubject = elem.IdSubject;
+                    // d.Text = elem.Text;
+
+                    // db.QuestionChoices.RemoveRange(db.QuestionChoices.Where(x => x.IdQuestion == elem.Id));
+                    // foreach(var choice in elem.Choices)
+                    // {
+                    //     QuestionChoice newqc = new()
+                    //     {
+                    //         IdChoice = choice.Id,
+                    //         IdQuestion = elem.Id
+                    //     };
+                    //     db.Add(newqc);
+                    // }
+
+                    // if (await db.SaveChangesAsync() > 0)
+                    // {
+                    //     modified.Add(elem.Id);
+                    // }
+                }
+            }
+        }
+        catch (Exception) { }
+
+        return modified;
+    }
+
+    public static async Task<List<int>> Hide(DB db, List<DocumentModel> documents)
+    {
+        List<int> hiddenItems = [];
+        try
+        {
+            foreach (var elem in documents)
+            {
+                var c = db.Documents.Where(x => x.Id == elem.Id).SingleOrDefault();
+                if (c is not null)
+                {
+                    //c.Active = false;
+                    if (await db.SaveChangesAsync() > 0)
                     {
-                        // d.IdCategory = elem.IdCategory;
-                        // d.IdSubject = elem.IdSubject;
-                        // d.Text = elem.Text;
-
-                        // db.QuestionChoices.RemoveRange(db.QuestionChoices.Where(x => x.IdQuestion == elem.Id));
-                        // foreach(var choice in elem.Choices)
-                        // {
-                        //     QuestionChoice newqc = new()
-                        //     {
-                        //         IdChoice = choice.Id,
-                        //         IdQuestion = elem.Id
-                        //     };
-                        //     db.Add(newqc);
-                        // }
-
-                        // if (await db.SaveChangesAsync() > 0)
-                        // {
-                        //     modified.Add(elem.Id);
-                        // }
+                        hiddenItems.Add(elem.Id);
                     }
                 }
             }
-            catch (Exception) { }
-
-            return modified;
         }
+        catch (Exception) { }
 
-        public static async Task<List<int>> Hide(DB db, List<DocumentModel> documents)
-        {
-            List<int> hiddenItems = [];
-            try
-            {
-                foreach (var elem in documents)
-                {
-                    var c = db.Documents.Where(x => x.Id == elem.Id).SingleOrDefault();
-                    if (c is not null)
-                    {
-                        //c.Active = false;
-                        if (await db.SaveChangesAsync() > 0)
-                        {
-                            hiddenItems.Add(elem.Id);
-                        }
-                    }
-                }
-            }
-            catch (Exception) { }
-
-            return hiddenItems;
-        }
+        return hiddenItems;
+    }
 }
