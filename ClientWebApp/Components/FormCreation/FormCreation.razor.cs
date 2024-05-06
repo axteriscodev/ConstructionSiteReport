07c9201a3da1efbffbeb022ba58c8e2a85ef6708
mmichelotti89 <mmichelotti@axterisco.it>
2024-05-06 17:15:03 +0200

    modifiche alla form di creazione del documento

--- ClientWebApp/Components/FormCreation/FormCreation.razor.cs
+++ ClientWebApp/Components/FormCreation/FormCreation.razor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using ClientWebApp.Repositories;
 using Microsoft.AspNetCore.Components;
+using Radzen;
 using Shared;
 
 namespace ClientWebApp.Components.FormCreation;
@@ -12,7 +13,9 @@ public partial class FormCreation
 
     //private IEnumerable<int> selected = [1,3,6];
 
-    private List<IdCategoryAndQuestions> selected = [];
+    private List<IdCategoryAndQuestions> groups = [];
+    private string title = "";
+    
 
     /// <summary>
     /// booleano che indica se la pagina sta eseguendo il caricamento iniziale
@@ -28,7 +31,12 @@ public partial class FormCreation
     /// Intero che ci dice quanti sono gli elementi
     /// </summary>
     private int count;
+    private int qCount = 0;
 
+    /// <summary>
+    /// il design degli elementi della form
+    /// </summary>
+    readonly Variant variant = Variant.Outlined;
 
     [Parameter]
     public string Param { get; set; } = "";
@@ -47,7 +55,9 @@ public partial class FormCreation
         count = categories.Count;
         foreach (var category in categories)
         {
-            selected.Add(new() { Id = category.Id, Text = category.Text, Questions = category.Questions });
+
+            OrderElements(category.Questions);
+            groups.Add(new() { Id = category.Id, Order=category.Order, Text = category.Text, Questions = category.Questions });
         }
     }
 
@@ -62,19 +72,19 @@ public partial class FormCreation
     {
         List<CategoryModel> documentCategories = [];
 
-        foreach (var selection in selected)
+        foreach (var group in groups)
         {
-            if (selection.SelectedQuestionIds.Any())
+            if (group.SelectedQuestionIds.Any())
             {
                 var category = new CategoryModel()
                 {
-                    Id = selection.Id,
-                    Text = selection.Text,
+                    Id = group.Id,
+                    Text = group.Text,
                 };
 
-                foreach (var selectedQuestionId in selection.SelectedQuestionIds)
+                foreach (var selectedQuestionId in group.SelectedQuestionIds)
                 {
-                    var selectedQuestion = selection.Questions.First(x => x.Id == selectedQuestionId);
+                    var selectedQuestion = group.Questions.First(x => x.Id == selectedQuestionId);
                     category.Questions.Add(selectedQuestion);
                 }
 
@@ -90,12 +100,108 @@ public partial class FormCreation
         await DocumentsRepository.SaveDocument(document);
     }
 
+    #region Visualizzazione
+
+    private string CategoryText(IdCategoryAndQuestions group)
+    {
+        return group.Order + ". " + group.Text;
+    }
+
+    private string QuestionText(IdCategoryAndQuestions group, string questionText, int order)
+    {
+        return group.Order + "."  + order + " " + questionText;
+    }
+
+    #endregion
+
+    #region Selezione categorie e domande 
+
+    private async Task<bool> ChangeCheckBoxCategory(bool? value, IdCategoryAndQuestions group)
+    {
+        value ??= true;
+        if (value.Value)
+        {
+            foreach (var q in group.Questions)
+            {
+                group.SelectedQuestionIds.Add(q.Id);
+            }
+        }
+        else
+        {
+            group.SelectedQuestionIds = [];
+        }
+        group.State = value.Value;
+        return false;
+    }
+
+    private bool ValoreCheckBoxQuestion(int id, IdCategoryAndQuestions group)
+    {
+        return group.SelectedQuestionIds.Contains(id);
+    }
+
+    private async Task<bool> ChangeCheckBoxQuestion(bool value, int id, IdCategoryAndQuestions group)
+    {
+        if (value)
+        {
+            group.SelectedQuestionIds.Add(id);
+            group.State = group.SelectedQuestionIds.Count == group.Questions.Count ? true : null;
+        }
+        else
+        {
+            group.SelectedQuestionIds.Remove(id);
+            group.State = group.SelectedQuestionIds.Count > 0 ? null : false;
+        }
+        return false;
+    }
+
+    #endregion
+
+    #region Ordinamento domande 
+
+    private void OrderList((int oldIndex, int newIndex, int groupNumber) indici)
+    {
+        // spezzo la tupla
+        var (oldIndex, newIndex, groupNumber) = indici;
+
+        var items = groups.Where(x=>x.Id == groupNumber).SingleOrDefault().Questions;
+        var itemToMove = items[oldIndex];
+        items.RemoveAt(oldIndex);
+
+        if (newIndex < items.Count)
+        {
+            items.Insert(newIndex, itemToMove);
+        }
+        else
+        {
+            items.Add(itemToMove);
+        }
+
+        OrderElements(items);
+        StateHasChanged();
+    }
+
+    private static void OrderElements(List<QuestionModel> lista)
+    {
+        for (int i = 0; i < lista.Count; i++)
+        {
+            lista[i].Order = i + 1;
+        }
+    }
+
+    #endregion
+
+    #region Classe interna 
+
+    class IdCategoryAndQuestions
+    {
+        public int Id { get; set; }
+        public int Order { get; set; }
+        public string Text { get; set; } = "";
+        public bool? State { get; set; } = false;
+        public List<int> SelectedQuestionIds { get; set; } = [];
+        public List<QuestionModel> Questions { get; set; } = [];
+    }
+
+    #endregion
 }
 
-class IdCategoryAndQuestions
-{
-    public int Id { get; set; }
-    public string Text { get; set; } = "";
-    public IEnumerable<int> SelectedQuestionIds { get; set; } = [];
-    public List<QuestionModel> Questions { get; set; } = [];
-}
