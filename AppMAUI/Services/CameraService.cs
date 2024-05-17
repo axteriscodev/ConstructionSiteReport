cb994212d9cbbc8d0ca307c24cedf83f5c3b9414
evaldinosi <evaldinosi@axterisco.it>
2024-05-17 16:32:17 +0200

    mac plist, home resize and column for picture, wip on text on image

--- AppMAUI/Services/CameraService.cs
+++ AppMAUI/Services/CameraService.cs
@@ -2,12 +2,12 @@
 using Microsoft.Maui.Graphics.Platform;
 using System.Reflection;
 using IImage = Microsoft.Maui.Graphics.IImage;
-
+using Microsoft.Maui.Graphics.Skia;
+using Font = Microsoft.Maui.Graphics.Font;
 namespace AppMAUI.Services
 {
     public class CameraService : ICameraService
     {
-
         public async Task<string> OpenCamera()
         {
             if (MediaPicker.Default.IsCaptureSupported)
@@ -28,8 +28,59 @@ namespace AppMAUI.Services
 
                     if (image != null)
                     {
-                        IImage newImage = image.Downsize(1000, true);
-                        var PhotoPath = string.Format("data:image/png;base64,{0}", newImage.AsBase64());
+                        IImage newImage = image.Downsize(1000, true); //TODO mettere 1000
+                        int height = (int)newImage.Height;
+                        int width = (int)newImage.Width;
+
+                        //Aggiunta testo all'immagine
+                        SkiaBitmapExportContext bmp = new(width: width, height: height, 1.0f);
+
+                        // bmp.Image.Draw(bmp.Canvas.DrawImage(newImage, 0, 0, width: width, height, height));
+
+                        // ICanvas imagecCanvas = bmp.Canvas;
+                        // imagecCanvas.DrawImage(newImage, x: 0, y: 0, width: width, height: height);
+
+                        // imagecCanvas.SetFillPaint(newImage.AsPaint(), RectF.Zero);
+                        // imagecCanvas.FillRectangle(0, 0, width, height);
+
+                        // imagecCanvas.FillRectangle(0, 0, width, height);
+
+                        // Rect myImageRectangle = new(x:0, y:0, width: width, height: height);
+                        // bmp.Image.Draw(imagecCanvas, myImageRectangle);
+
+                        // PathF path = new PathF();
+
+                        ICanvas writeCanvas = bmp.Canvas;
+
+                        // Measure a string
+                        string myText = "Hello, Maui.Graphics!";
+                        Font myFont = new("Impact");
+                        float myFontSize = 48;
+                        writeCanvas.Font = myFont;
+                        SizeF textSize = writeCanvas.GetStringSize(myText, myFont, myFontSize);
+
+                        // Draw a rectangle to hold the string
+                        Point point = new(
+                            x: (bmp.Width - textSize.Width) / 2,
+                            y: (bmp.Height - textSize.Height) / 2);
+                        Rect myTextRectangle = new(point, textSize);
+
+                        // Daw the string itself
+                        writeCanvas.FontSize = myFontSize * .9f; // smaller than the rectangle
+                        writeCanvas.FontColor = Colors.White;
+                        writeCanvas.DrawString(myText, myTextRectangle,
+                            HorizontalAlignment.Center, VerticalAlignment.Center, TextFlow.OverflowBounds);
+
+                        // bmp.WriteToFile(localFilePath);
+                        // Stream bmpStream = Stream.Null;
+                        // bmp.WriteToStream(bmpStream);
+                        // bmp.Image.AsBase64
+
+                        // IImage writtenImage;
+                        // writtenImage = PlatformImage.FromStream(bmpStream);
+                        // var PhotoPath = string.Format("data:image/png;base64,{0}", newImage.AsBase64());
+
+                        var PhotoPath = string.Format("data:image/png;base64,{0}", bmp.Image.AsBase64());
 
                         return PhotoPath;
                     }
