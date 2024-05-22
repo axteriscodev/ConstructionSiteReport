3499e7f459402436f1f51c5f8ea1826b8f79919a
Giascar <giascar82@gmail.com>
2024-05-22 17:34:10 +0200

    clients e companies repository

---
+++ ConstructionSiteLibrary/Repositories/ClientsRepository.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+using ConstructionSiteLibrary.Managers;
+using Shared;
+
+namespace ConstructionSiteLibrary.Repositories;
+
+public class ClientsRepository(HttpManager httpManager)
+{
+    List<ClientModel> Clients = [];
+
+    private HttpManager _httpManager = httpManager;
+
+    public async Task<List<ClientModel>> GetClients()
+    {
+        if (Clients.Count == 0)
+        {
+            var response = await _httpManager.SendHttpRequest("Client/ClientsList", "");
+            if (response.Code.Equals("0"))
+            {
+                Clients = JsonSerializer.Deserialize<List<ClientModel>>(response.Content.ToString() ?? "") ?? [];
+            }
+        }
+
+        return Clients;
+    }
+
+    public async Task<bool> SaveClient(ClientModel client)
+    {
+        var response = await _httpManager.SendHttpRequest("Client/SaveClient", client);
+
+        //NotificationService.Notify(response);
+        if (response.Code.Equals("0"))
+        {
+            Clients.Clear();
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<bool> UpdateClients(List<ClientModel> clients)
+    {
+        var response = await _httpManager.SendHttpRequest("Client/UpdateClients", clients);
+        //NotificationService.Notify(response);
+        if (response.Code.Equals("0"))
+        {
+            Clients.Clear();
+            return true;
+        }
+
+        return false;
+    }
+
+    
+    public async Task<bool> HideClients(List<ClientModel> clients)
+    {
+        var response = await _httpManager.SendHttpRequest("Client/HideClients", clients);
+        //NotificationService.Notify(response);
+        if (response.Code.Equals("0"))
+        {
+            Clients.Clear();
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ ConstructionSiteLibrary/Repositories/CompaniesRepository.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using ConstructionSiteLibrary.Managers;
+using Shared;
+
+namespace ConstructionSiteLibrary.Repositories;
+
+public class CompaniesRepository(HttpManager httpManager)
+{
+    List<CompanyModel> Companies = [];
+
+    private HttpManager _httpManager = httpManager;
+
+    public async Task<List<CompanyModel>> GetCompanies()
+    {
+        if (Companies.Count == 0)
+        {
+            var response = await _httpManager.SendHttpRequest("Company/CompaniesList", "");
+            if (response.Code.Equals("0"))
+            {
+                Companies = JsonSerializer.Deserialize<List<CompanyModel>>(response.Content.ToString() ?? "") ?? [];
+            }
+        }
+
+        return Companies;
+    }
+
+    public async Task<bool> SaveCompany(CompanyModel company)
+    {
+        var response = await _httpManager.SendHttpRequest("Company/SaveCompany", company);
+
+        //NotificationService.Notify(response);
+        if (response.Code.Equals("0"))
+        {
+            Companies.Clear();
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<bool> UpdateCompanies(List<CompanyModel> companies)
+    {
+        var response = await _httpManager.SendHttpRequest("Company/UpdateCompanies", companies);
+        //NotificationService.Notify(response);
+        if (response.Code.Equals("0"))
+        {
+            Companies.Clear();
+            return true;
+        }
+
+        return false;
+    }
+
+    
+    public async Task<bool> HideCompanies(List<CompanyModel> companies)
+    {
+        var response = await _httpManager.SendHttpRequest("Company/HideCompanies", companies);
+        //NotificationService.Notify(response);
+        if (response.Code.Equals("0"))
+        {
+            Companies.Clear();
+            return true;
+        }
+
+        return false;
+    }
+}
+
