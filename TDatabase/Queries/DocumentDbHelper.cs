43106ecf0d72613de3b28eb831263ff74c8b3498
Giascar <giascar82@gmail.com>
2024-05-29 16:38:31 +0200

    DocumentDbHelper - Insert

--- TDatabase/Queries/DocumentDbHelper.cs
+++ TDatabase/Queries/DocumentDbHelper.cs
@@ -42,7 +42,7 @@ public class DocumentDbHelper
                                           Text = c.Text,
                                           Order = c.Order,
                                           Questions = (from tc in db.Templates
-                                                        from qc in db.QuestionChosens
+                                                       from qc in db.QuestionChosens
                                                        from q in db.Questions
                                                        where tc.Id == d.IdTemplate
                                                        && qc.IdTemplate == tc.Id
@@ -60,7 +60,7 @@ public class DocumentDbHelper
                                                                             where qa.IdDocument == d.Id
                                                                             && qa.IdQuestionChosen == qc.Id
                                                                             && cc.Id == qa.IdCurrentChoice
-                                                                            select new DocumentChoiceModel() 
+                                                                            select new DocumentChoiceModel()
                                                                             {
                                                                                 Id = cc.Id,
                                                                                 Value = cc.Value,
@@ -72,7 +72,7 @@ public class DocumentDbHelper
                                                                                                       && rc.IdDocument == d.Id
                                                                                                       select rc.IdCompany).ToList(),
                                                                             }).ToList(),
-                                                            Choices = (
+                                                           Choices = (
                                                              from qch in db.QuestionChoices
                                                              from ch in db.Choices
                                                              where qch.IdQuestion == q.Id
@@ -80,43 +80,43 @@ public class DocumentDbHelper
                                                              && ch.Active == true
                                                              select new DocumentChoiceModel()
                                                              {
-                                                                Id = ch.Id,
-                                                                Value = ch.Value,
-                                                                Reportable = ch.Reportable,
-                                                                Color = ch.Color,
-                                                                
+                                                                 Id = ch.Id,
+                                                                 Value = ch.Value,
+                                                                 Reportable = ch.Reportable,
+                                                                 Color = ch.Color,
+
                                                              }).ToList(),
 
-                                                             Attachments = (from aq in db.AttachmentQuestions
-                                                                            from att in db.Attachments
-                                                                            where aq.IdQuestion == qc.Id
-                                                                            && att.IdDocument == d.Id
-                                                                            && att.Id == aq.IdAttachment
-                                                                            select new AttachmentModel()
-                                                                            {
-                                                                                Id = att.Id,
-                                                                                Name = att.Name,
-                                                                                Date = att.DateTime,
-                                                                                //TODO manca il binding alla path
-                                                                            }).ToList(),
+                                                           Attachments = (from aq in db.AttachmentQuestions
+                                                                          from att in db.Attachments
+                                                                          where aq.IdQuestion == qc.Id
+                                                                          && att.IdDocument == d.Id
+                                                                          && att.Id == aq.IdAttachment
+                                                                          select new AttachmentModel()
+                                                                          {
+                                                                              Id = att.Id,
+                                                                              Name = att.Name,
+                                                                              Date = att.DateTime,
+                                                                              //TODO manca il binding alla path
+                                                                          }).ToList(),
                                                        }).Cast<IQuestion>().ToList()
                                       }).ToList(),
                         ConstructorSite = (from cs in db.ConstructorSites
-                                          where cs.Id == d.IdConstructorSite
-                                          select new ConstructorSiteModel()
-                                          {
-                                              Id  = cs.Id,
-                                              JobDescription = cs.JobDescription,
-                                              StartDate = cs.StartDate,
-                                              Address = cs.Address,
-                                              Client = (from cl in db.Clients
-                                                        where cl.Id == cs.IdClient
-                                                        select new ClientModel()
-                                                        {
-                                                            Id = cl.Id,
-                                                            Name = cl.Name
-                                                        }).SingleOrDefault() ?? new(),
-                                          }).SingleOrDefault(),
+                                           where cs.Id == d.IdConstructorSite
+                                           select new ConstructorSiteModel()
+                                           {
+                                               Id = cs.Id,
+                                               JobDescription = cs.JobDescription,
+                                               StartDate = cs.StartDate,
+                                               Address = cs.Address,
+                                               Client = (from cl in db.Clients
+                                                         where cl.Id == cs.IdClient
+                                                         select new ClientModel()
+                                                         {
+                                                             Id = cl.Id,
+                                                             Name = cl.Name
+                                                         }).SingleOrDefault() ?? new(),
+                                           }).SingleOrDefault(),
                         Client = (from cl in db.Clients
                                   where cl.Id == d.IdClient
                                   select new ClientModel()
@@ -126,17 +126,17 @@ public class DocumentDbHelper
                                   }).SingleOrDefault(),
 
                         Companies = (from compDoc in db.CompanyDocuments
-                                    from comp in db.Companies
-                                    where compDoc.IdDocument == d.Id
-                                    && comp.Id == compDoc.IdCompany
-                                    select new CompanyModel()
-                                    {   
-                                        Id = comp.Id,
-                                        Name = comp.Name,
-                                        Address = comp.Address ?? "",
-                                        VatCode = comp.Vatcode,
-                                        Present = compDoc.Present
-                                    }).ToList()
+                                     from comp in db.Companies
+                                     where compDoc.IdDocument == d.Id
+                                     && comp.Id == compDoc.IdCompany
+                                     select new CompanyModel()
+                                     {
+                                         Id = comp.Id,
+                                         Name = comp.Name,
+                                         Address = comp.Address ?? "",
+                                         VatCode = comp.Vatcode,
+                                         Present = compDoc.Present
+                                     }).ToList()
                     }).ToList();
 
         return docs;
@@ -147,52 +147,93 @@ public class DocumentDbHelper
 
         //TODO associare template
 
-        // var documentId = 0;
-        // try
-        // {
-        //     var nextId = (db.Documents.Any() ? db.Documents.Max(x => x.Id) : 0) + 1;
-        //     Document newDocument = new()
-        //     {
-        //         Id = nextId,
-        //         IdConstructorSite = document.ConstructorSite?.Id,
-        //         IdClient = document.Client?.Id,
-        //         Title = document.Title,
-        //         //Date = document.Date,
-        //         //Active = true,
-        //     };
+        var documentId = 0;
+        try
+        {
+            var nextId = (db.Documents.Any() ? db.Documents.Max(x => x.Id) : 0) + 1;
+            Document newDocument = new()
+            {
+                Id = nextId,
+                IdConstructorSite = document.ConstructorSite?.Id,
+                IdClient = document.Client?.Id,
+                CreationDate = document.CreationDate,
+                LastEditDate = document.LastEditDate,
+                CompilationDate = document.CompilationDate,
+                Title = document.Title,
+                ReadOnly = document.ReadOnly,
+            };
 
-        //     db.Documents.Add(newDocument);
+            db.Documents.Add(newDocument);
 
-        //     var nextQuestionChosenId = (db.QuestionChosens.Any() ? db.QuestionChosens.Max(x => x.Id) : 0) + 1;
+            foreach (var companyDoc in document.Companies)
+            {
+                CompanyDocument cd = new()
+                {
+                    IdCompany = companyDoc.Id,
+                    IdDocument = document.Id,
+                    Present = companyDoc.Present ?? false,
+                };
 
-        //     foreach (var c in document.Categories)
-        //     {
-        //         foreach (DocumentQuestionModel q in c.Questions)
-        //         {
-        //             QuestionChosen qc = new()
-        //             {
-        //                 Id = nextQuestionChosenId,
-        //               //  IdDocument = nextId,
-        //                 IdQuestion = q.Id,
-        //                 Order = q.Order,
-        //                // Printable = true,
-        //                // Hidden = false
-        //             };
-        //             db.QuestionChosens.Add(qc);
-        //             nextQuestionChosenId++;
-        //         }
+                db.CompanyDocuments.Add(cd);
+            }
 
-        //     }
-        //     await db.SaveChangesAsync();
-        //     documentId = nextId;
-        // }
-        // catch (Exception e) {
-        //     var ex = e.Message;
-        //  }
+            foreach (var cat in document.Categories)
+            {
+                foreach (var q in cat.Questions.Cast<DocumentQuestionModel>())
+                {
+                    foreach (var cc in q.CurrentChoices)
+                    {
+                        QuestionAnswered qc = new()
+                        {
+                            IdDocument = document.Id,
+                            IdCurrentChoice = cc.Id,
+                            IdQuestionChosen = q.Id
+                        };
+                        db.QuestionAnswereds.Add(qc);
 
-        //return documentId;
+                        foreach (var rci in cc.ReportedCompanyIds)
+                        {
+                            ReportedCompany rc = new()
+                            {
+                                IdCompany = rci,
+                                IdDocument = document.Id,
+                                IdCurrentChoice = cc.Id,
+                                IdQuestionChosen = q.Id
+                            };
+                            db.ReportedCompanies.Add(rc);
+                        }
+                    }
 
-        return 0;
+                    foreach(var attach in q.Attachments)
+                    {
+                        var nextAttachId =(db.Attachments.Any() ? db.Attachments.Max(x => x.Id) : 0) + 1; 
+
+                        Attachment attachment = new()
+                        {
+                            Id = nextAttachId,
+                            IdDocument = document.Id,
+                            DateTime = attach.Date,
+                        };
+                        db.Attachments.Add(attachment);
+
+                        AttachmentQuestion attachmentQuestion = new()
+                        {
+                            IdAttachment = nextAttachId,
+                            IdQuestion = q.Id,
+                        };
+                        db.AttachmentQuestions.Add(attachmentQuestion);
+                    }
+                }
+            }
+            await db.SaveChangesAsync();
+            documentId = nextId;
+        }
+        catch (Exception e)
+        {
+            var ex = e.Message;
+        }
+
+        return documentId;
     }
 
     public static async Task<List<int>> Update(DB db, List<DocumentModel> documents)
@@ -203,46 +244,46 @@ public class DocumentDbHelper
             foreach (var document in documents)
             {
                 var d = db.Documents.Where(x => x.Id == document.Id).FirstOrDefault();
-           /*     if (d is not null && CheckLastEdit(d.LastModified, document.LastModified!.Value))
-                {
-                  //  d.LastModified = document.LastModified;
-                    foreach (var c in document.Categories)
-                    {
-                        foreach(var q in c.Questions)
-                        {
-                            var qc = db.QuestionChosens.Where(x => x.IdDocument == document.Id && x.IdQuestion == q.Id).FirstOrDefault();
-                            if(qc is not null)
-                            {
-                                qc.IdCurrentChoice = q.CurrentChoice?.Id;
-                                qc.Order = q.Order;
-                                qc.Printable = q.Printable;
-                                qc.Hidden = q.Hidden;
-                                await db.SaveChangesAsync();
-                                modified.Add(qc.Id);
-                            }
-                        }
-                    }
+                /*     if (d is not null && CheckLastEdit(d.LastModified, document.LastModified!.Value))
+                     {
+                       //  d.LastModified = document.LastModified;
+                         foreach (var c in document.Categories)
+                         {
+                             foreach(var q in c.Questions)
+                             {
+                                 var qc = db.QuestionChosens.Where(x => x.IdDocument == document.Id && x.IdQuestion == q.Id).FirstOrDefault();
+                                 if(qc is not null)
+                                 {
+                                     qc.IdCurrentChoice = q.CurrentChoice?.Id;
+                                     qc.Order = q.Order;
+                                     qc.Printable = q.Printable;
+                                     qc.Hidden = q.Hidden;
+                                     await db.SaveChangesAsync();
+                                     modified.Add(qc.Id);
+                                 }
+                             }
+                         }
 
-                    // d.IdCategory = elem.IdCategory;
-                    // d.IdSubject = elem.IdSubject;
-                    // d.Text = elem.Text;
+                         // d.IdCategory = elem.IdCategory;
+                         // d.IdSubject = elem.IdSubject;
+                         // d.Text = elem.Text;
 
-                    // db.QuestionChoices.RemoveRange(db.QuestionChoices.Where(x => x.IdQuestion == elem.Id));
-                    // foreach(var choice in elem.Choices)
-                    // {
-                    //     QuestionChoice newqc = new()
-                    //     {
-                    //         IdChoice = choice.Id,
-                    //         IdQuestion = elem.Id
-                    //     };
-                    //     db.Add(newqc);
-                    // }
+                         // db.QuestionChoices.RemoveRange(db.QuestionChoices.Where(x => x.IdQuestion == elem.Id));
+                         // foreach(var choice in elem.Choices)
+                         // {
+                         //     QuestionChoice newqc = new()
+                         //     {
+                         //         IdChoice = choice.Id,
+                         //         IdQuestion = elem.Id
+                         //     };
+                         //     db.Add(newqc);
+                         // }
 
-                    // if (await db.SaveChangesAsync() > 0)
-                    // {
-                    //     modified.Add(elem.Id);
-                    // }
-                } */
+                         // if (await db.SaveChangesAsync() > 0)
+                         // {
+                         //     modified.Add(elem.Id);
+                         // }
+                     } */
             }
         }
         catch (Exception) { }
@@ -260,7 +301,7 @@ public class DocumentDbHelper
                 var c = db.Documents.Where(x => x.Id == elem.Id).SingleOrDefault();
                 if (c is not null)
                 {
-                   // c.Active = false;
+                    // c.Active = false;
                     if (await db.SaveChangesAsync() > 0)
                     {
                         hiddenItems.Add(elem.Id);
