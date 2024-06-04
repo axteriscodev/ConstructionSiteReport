f92c7cad8a9415cdb0e6de8d8a269126a09092af
mmichelotti89 <mmichelotti@axterisco.it>
2024-06-04 18:04:54 +0200

    modifica alle card del constructorSite

---
+++ ConstructionSiteLibrary/Components/ConstructorSites/FormConstructorSite.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shared.Documents;
+
+namespace ConstructionSiteLibrary.Components.ConstructorSites
+{
+    public partial class FormConstructorSite
+    {
+        [Parameter]
+        public EventCallback OnSaveComplete { get; set; }
+        [Parameter]
+        public bool CreationMode { get; set; }
+        [Parameter]
+        public ConstructorSiteModel? Site { get; set; }
+
+        private FormSite form = new();
+
+        private bool onSaving = false;
+        private bool onLoading = false;
+
+
+        private async Task Save()
+        {
+            Site = new()
+            {
+                Name = form.Name!,
+                JobDescription = form.JobDescription ?? "",
+                Address = form.Address!,
+                StartDate = form.StartDate.HasValue ? form.StartDate.Value : DateTime.Today,
+            };
+            var success = await SiteRepository.SaveContructorSite(Site);
+            if(success)
+            {
+                await OnSaveComplete.InvokeAsync();
+            }
+        }
+
+        private class FormSite
+        {
+            public string? Name { get; set; }
+            public string? JobDescription { get; set; }
+            public string? Address { get; set; }
+            public DateTime? StartDate { get; set; }
+        }
+    }
+}
