c28d7dd70d56c1ee7a6267f2236543ff3b38f142
Giascar <giascar82@gmail.com>
2024-06-05 11:31:28 +0200

    updated company data model

--- Shared/Documents/CompanyModel.cs
+++ Shared/Documents/CompanyModel.cs
@@ -7,15 +7,48 @@ public class CompanyModel
     [JsonPropertyName("Id")]
     public int Id { get; set; }
 
-    [JsonPropertyName("name")]
-    public string Name { get; set; } = "";
+    [JsonPropertyName("companyName")]
+    public string CompanyName { get; set; } = "";
+
+    [JsonPropertyName("selfEmployedName")]
+    public string SelfEmployedName { get; set; } = "";
 
     [JsonPropertyName("address")]
     public string Address { get; set; } = "";
 
+    [JsonPropertyName("taxId")]
+    public string TaxId { get; set; } = "";
+
     [JsonPropertyName("vatCode")]
     public string VatCode { get; set; } = "";
 
-    [JsonPropertyName("presente")]
+    [JsonPropertyName("phone")]
+    public string Phone { get; set; } = "";
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; } = "";
+
+    [JsonPropertyName("pec")]
+    public string Pec { get; set; } = "";
+
+    [JsonPropertyName("reaNumber")]
+    public string ReaNumber { get; set; } = "";
+
+    [JsonPropertyName("workerWelfareFunds")]
+    public string WorkerWelfareFunds { get; set; } = "";
+
+    [JsonPropertyName("ccnl")]
+    public string Ccnl { get; set; } = "";
+
+    [JsonPropertyName("inpsId")]
+    public string InpsId { get; set; } = "";
+
+    [JsonPropertyName("inpsPat")]
+    public string InpsPat { get; set; } = "";
+
+    [JsonPropertyName("jobsDescriptions")]
+    public string JobsDescriptions { get; set; } = "";
+
+    [JsonPropertyName("present")]
     public bool? Present { get; set; }
 }
