e00109ff03c02d7abee27eeb15e862a17d6fff6e
Giascar <giascar82@gmail.com>
2024-06-11 12:03:59 +0200

    template steps - init

---
+++ ConstructionSiteLibrary/Components/Templates/Wizard/TemplateSelectionStep.razor.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Templates;
+
+namespace ConstructionSiteLibrary.Components.Templates.Wizard;
+
+public partial class TemplateSelectionStep
+{
+    List<TemplateModel> Templates = [];
+
+    TemplateModel? CurrentSelection;
+
+    /// <summary>
+    /// booleano che indica se la pagina sta eseguendo il caricamento iniziale
+    /// </summary>
+    private bool initialLoading;
+
+    ScreenComponent screenComponent;
+
+}
---
+++ ConstructionSiteLibrary/Components/Templates/Wizard/WizardTemplateCreation.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace ConstructionSiteLibrary.Components.Templates.Wizard;
+
+public partial class WizardTemplateCreation
+{
+    /// <summary>
+    /// booleano che indica se la pagina sta eseguendo il caricamento iniziale
+    /// </summary>
+    private bool initialLoading;
+
+    ScreenComponent screenComponent;
+}
