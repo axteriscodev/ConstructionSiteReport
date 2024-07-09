abc1cc6d0f5d111ba5f5beb6f428b4776b8f6e19
Giascar <giascar82@gmail.com>
2024-07-09 17:54:23 +0200

    test dialog via injection

--- ClientWebApp/Services/CameraService.cs
+++ ClientWebApp/Services/CameraService.cs
@@ -1,11 +1,19 @@
-﻿using ConstructionSiteLibrary.Interfaces;
+﻿using ConstructionSiteLibrary.Components.Choices;
+using ConstructionSiteLibrary.Interfaces;
+using Radzen;
+
 
 namespace ClientWebApp.Services
 {
-    public class CameraService : ICameraService
+    public class CameraService(DialogService dialogService) : ICameraService
     {
+
+        private DialogService _dialogService = dialogService;
+
         public async Task<string> OpenCamera()
         {
+            await _dialogService.Confirm("", "titolo",
+            new ConfirmOptions { OkButtonText = "Si", CancelButtonText = "No" });
             return "";
         }
     }
