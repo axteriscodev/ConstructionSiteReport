7d76848b8b7e8599045717abc9ae17e09151a5a3
mmichelotti89 <mmichelotti@axterisco.it>
2024-09-11 17:48:24 +0200

    creato componente per la navigazione nel documento

---
+++ ConstructionSiteLibrary/Model/DocumentCompilation/DocumentNavigator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionSiteLibrary.Model.DocumentCompilation
+{
+    public class DocumentNavigator
+    {
+        public delegate void AnchorsChanged(object sender);
+        public event AnchorsChanged OnAnchorsChanged;
+        public List<DocumentAnchor> Anchors { get; set; } = [];
+
+        public void AddAnchor(DocumentAnchor newAnchor)
+        {
+            // se l'ancora non è nella lista la aggiungo
+            if (!Anchors.Exists(x => x.Anchor.Equals(newAnchor.Anchor)))
+            {
+                Anchors.Add(newAnchor);
+                Anchors = Anchors.OrderBy(x => x.TypeIndex).ThenBy(x => x.Index).ToList();
+                OnAnchorsChanged?.Invoke(this);
+            }
+
+        }
+
+
+
+
+
+    }
+
+}
