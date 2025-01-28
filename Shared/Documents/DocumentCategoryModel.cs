db44a8674f516520c4b7472f8360cc245b0c96f2
mmichelotti89 <mmichelotti@axterisco.it>
2025-01-28 11:17:54 +0100

    aggiunto campo printable alle DocumentCategoryModel

--- Shared/Documents/DocumentCategoryModel.cs
+++ Shared/Documents/DocumentCategoryModel.cs
@@ -10,6 +10,8 @@ namespace Shared.Documents
 {
     public class DocumentCategoryModel : CategoryModel
     {
+        [JsonPropertyName("printable")]
+        public bool Printable { get; set; }
         [JsonPropertyName("questions")]
         public List<DocumentQuestionModel> Questions { get; set; } = [];
     }
