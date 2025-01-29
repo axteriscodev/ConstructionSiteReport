272e090f1b64555b075391ef12031cccaa42aa15
mmichelotti89 <mmichelotti@axterisco.it>
2025-01-29 16:17:28 +0100

    modifiche alla stampa pdf

--- ConstructionSiteLibrary/wwwroot/pdfManager.js
+++ ConstructionSiteLibrary/wwwroot/pdfManager.js
@@ -24,60 +24,29 @@ export async function generaPDFDocumento(
             doc.setPage(i);
 
             let x = 15; //posizione inizio disegno
-            let y = 20; //posizione inizio disegno
+            let y = 12; //posizione inizio disegno
             let rectx = 100; //Larghezza rettangolo 
-            let recty = 15; //Altezza rettangolo
+            let recty = 10; //Altezza rettangolo
 
             const pageSize = doc.internal.pageSize;
             const pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
             const pageHeight = pageSize.height ? pageSize.height : pageSize.getHeight();
 
-            //Documento
-            doc.setDrawColor(192, 192, 192);
-            doc.setFillColor(210, 210, 210);
-            doc.rect(x, y, rectx, recty, 'FD');
-            doc.text("Documento", x + 10, y + 10);
-            doc.setFillColor(255, 255, 255,);
-            doc.rect(x, y + recty, rectx, recty, 'FD');
-            doc.text(documentString, x + 10, y + recty + 10);
+            doc.setFontSize(8);
+            doc.setTextColor(120, 120, 120);
+            //Documento e 
+            doc.text(documentString, x + 10, y + 10);
+            doc.text(compilatorString, x + 10, y + recty + 10);
 
-            //Nominativo compilatore
-            doc.setDrawColor(192, 192, 192);
-            doc.setFillColor(210, 210, 210);
-            doc.rect(x + rectx, y, rectx, recty, 'FD');
-            doc.text("Compilatore", x + rectx + 10, y + 10);
-            doc.setFillColor(255, 255, 255,);
-            doc.rect(x + rectx, y + recty, rectx, recty, 'FD');
-            doc.text(compilatorString, x + rectx + 10, y + recty + 10);
-
-            //Committente
-            let xTitleComm = pageWidth - rectx * 2 - x;
-            doc.setDrawColor(192, 192, 192);
-            doc.setFillColor(210, 210, 210);
-            doc.rect(xTitleComm, y, rectx, recty, 'FD');
-            doc.text("Committente", xTitleComm + 10, y + 10);
-            doc.setFillColor(255, 255, 255,);
-            doc.rect(xTitleComm, y + recty, rectx, recty, 'FD');
-            doc.text(committenteString, xTitleComm + 10, y + recty + 10);
-
-            //Indirizzo cantiere
-            let xTitleIndi = pageWidth - rectx - x;
-            doc.setDrawColor(192, 192, 192);
-            doc.setFillColor(210, 210, 210);
-            doc.rect(xTitleIndi, y, rectx, recty, 'FD');
-            doc.text("Indirizzo cantiere", xTitleIndi + 10, y + 10);
-            doc.setFillColor(255, 255, 255,);
-            doc.rect(xTitleIndi, y + recty, rectx, recty, 'FD');
-            doc.text(indirizzoCantiereString, xTitleIndi + 10, y + recty + 10);
+            //Committente ed indirizzo
+            let xTitleComm = (pageWidth / 2) - 2;
+            doc.text("Committente:" + committenteString, xTitleComm + 10, y + 10);
+            doc.text("Indirizzo cantiere:" + indirizzoCantiereString, xTitleComm + 10, y + recty + 10);
 
             const footer = `Page ${i} of ${pageCount}`;
 
             console.log("pdfLog - pages: " + pageCount + ", actual: " + i);
-            // console.log("pdfLog - pageWidth: " + pageWidth);
-            // console.log("pdfLog - pageHeight: " + pageHeight);
             console.log("pdfLog - footer: " + footer);
-            // console.log("pdfLog - alt: " + pageHeight - 15);
-            // console.log("pdfLog - largh: " + pageWidth / 2 - (doc.getTextWidth(footer) / 2));
 
             // Footer
             doc.text(footer, pageWidth / 2 - (doc.getTextWidth(footer) / 2), pageHeight - 15, {
@@ -90,7 +59,8 @@ export async function generaPDFDocumento(
     console.log("pdfLog - content offsetHeight: " + content.offsetHeight);
     //seleziono gli elementi che 
     const elements = document.querySelectorAll('.pdfElement');
-    let headerY = 50; // Altezza header
+    let headerY = 30; // Altezza header
+    let footerY = 40 // altezza footer
     let yPosition = 15 + headerY;
     let xPosition = 12;
     let pageCount = 0;
@@ -98,7 +68,7 @@ export async function generaPDFDocumento(
     let pageHeight = doc.internal.pageSize.getHeight();
     let pages = [];
     console.log("pdfLog - ph= " + pageHeight);
-    let max = 1350 - headerY;
+    let max = 1250 - headerY - footerY;
 
     let tempDiv = document.createElement('div');
     pages.push(tempDiv);
